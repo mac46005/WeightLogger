3bed645fef218bfb056480e4dae3c08310ebcd89
Marco Preciado <mac46005@gmail.com>
2023-02-08 21:53:37 -0600

    Added class lib and maui project to solution

---
+++ WeightLogger_UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WeightLogger_UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WeightLogger_UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WeightLogger_UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

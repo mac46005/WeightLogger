178bfd08d06aff7c95f31c9a5ab11ae1ad0cc196
Marco Preciado <mac46005@gmail.com>
2023-02-09 11:45:41 -0600

    IModelViewModel

---
+++ WeightLogger_UI/MVVM/ViewModels/Interfaces/IModelViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBManipulation_ClassLib.Interfaces;
+
+namespace WeightLogger_UI.MVVM.ViewModels.Interfaces
+{
+    public interface IModelViewModel<T,U> where T : IModel<U>
+    {
+        /// <summary>
+        /// The model that represents this view model of type IModel
+        /// </summary>
+        T Model { get; set; }
+    }
+}

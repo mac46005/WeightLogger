d2b15df923c367bd1055afa8258d5d3d9cd39ef4
Marco Preciado <mac46005@gmail.com>
2023-02-09 23:26:42 -0600

    CRUD table access

---
+++ DBManipulation_ClassLib/Interfaces/ITableAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBManipulation_ClassLib.Interfaces
+{
+    public interface ITableAccess<T,U>
+    {
+        Task<int> Create(T obj);
+        Task<int> Delete(U id);
+        Task<List<T>> Read();
+        Task<T> Read(U id);
+        Task<int> Update(T obj);
+    }
+}
